ea9f4fc196c0d03f7f53607ece584750c11f46b9
marcormsiva <marcoruan10@hotmail.com>
2026-09-10 22:45:49 -0300

    feat: adiciona o componente Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Header } from "./components/Header"
+import { Hero } from "./components/Hero"
 
 function App() {
-  
+
 
   return (
     <>
-  <Header />
+      <Header />
+      <main className="py-10">
+        <Hero />
+      </main>
 
     </>
   )
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,24 @@
+import Banner from "@/assets/images/banner.jpg"
+export const Hero = () => {
+    return (
+        <>
+            <div className="container">
+                <section className="relative h-125 rounde-[20px] mb-10">
+                    <img src={Banner} alt="Homem sentado com tênis da SytaxWear" className="w-full h-full object-cover rounded-[20px]" />
+
+                    <div className="absolute w-full bottom-0 flex justify-end items-center text-center px-6 md:px-24 pb-32">
+                        <div className="text-white w-91 flex flex-col items-center ">
+                            <h2 className="text-xl font-medium leading-normal tracking-wider mb-2.5">Krypton One</h2>
+                            <h1 className="text-2xl leading-9 tracking-wider mb-10">Transforme qualquer passo em presença</h1>
+
+                            <div className="flex gap-3.5">
+                                <button>Ver modelos</button>
+                                <button>Comprar</button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </>
+    )
+}
